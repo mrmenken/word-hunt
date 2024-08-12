8d2693c5b610dab00e73ed1f7d882d5031e9fad6
Maarten Menken <mrmenken@hotmail.com>
2024-08-12 18:08:57 +0200

    Add separate list of simple words

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { WordInputComponent } from "../word-input/word-input.component";
+import { WordInputComponent } from '../word-input/word-input.component';
+import { WordService } from '../../services/word.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WordInputComponent],
+  imports: [CommonModule, WordInputComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
+  protected readonly randomWord;
 
+  constructor(wordService: WordService) {
+    this.randomWord = wordService.randomWord(5);
+  }
 }
--- src/app/services/word.service.ts
+++ src/app/services/word.service.ts
@@ -1,34 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WordService {
-  private words: ReadonlySet<string> = new Set();
+  private allWords: ReadonlySet<string> = new Set();
+  private simpleWords = new ReplaySubject<ReadonlyArray<string>>(1);
 
   constructor(private http: HttpClient) {
-    this.loadWords();
+    this.loadWords('words.all.nl.txt').subscribe(
+      (words) => (this.allWords = new Set(words))
+    );
+
+    this.loadWords('words.simple.nl.txt').subscribe((words) =>
+      this.simpleWords.next(words)
+    );
+  }
+
+  randomWord(length: number) {
+    return this.simpleWords.pipe(
+      map((words) => {
+        const wordsOfLength = words.filter((word) => word.length === length);
+
+        if (wordsOfLength.length === 0)
+          throw new Error(`No words of length ${length} found`);
+
+        const index = Math.floor(Math.random() * wordsOfLength.length);
+        return wordsOfLength[index];
+      })
+    );
   }
 
   isValid(word: string) {
     word = word.toLowerCase();
-    return this.words.has(word);
+    return this.allWords.has(word);
   }
 
-  private loadWords() {
-    this.http
-      .get('words.nl.txt', { responseType: 'text' })
-      .pipe(
-        map(
-          (data) => new Set(data.split('\n').map((word) => word.toLowerCase()))
-        ),
-        catchError((error) => {
-          console.error('Error loading words:', error);
-          return [];
-        })
-      )
-      .subscribe((words) => (this.words = words));
+  private loadWords(file: string) {
+    return this.http.get(file, { responseType: 'text' }).pipe(
+      map((data) => data.split('\n').map((word) => word.toLowerCase())),
+      catchError((error) => {
+        console.error('Error loading words:', error);
+        return [];
+      })
+    );
   }
 }
